3bb02fd3a00b46241e5f7a41f5f0516805b7e104
swlm555 <1438199079@qq.com>
2025-11-08 12:45:06 +0800

    Create 20main.c

---
+++ 20main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a[10],i,j,t;
+    for(i=0;i<10;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<9;i++)
+        for(j=0;j<9-i;j++)
+            if(a[j]>a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+    for(i=0;i<10;i++)
+        printf("%d ",a[i]);
+    return 0;
+}

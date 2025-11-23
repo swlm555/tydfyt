869ce4fd4372c6f881750e86e759627075a43ae4
swlm555 <1438199079@qq.com>
2025-11-23 17:09:59 +0800

    Create 552main.c

---
+++ 552main.c
@@ -0,0 +1,20 @@
+//202510111229
+//1438199079@qq.com
+// 罗忠航
+#include <stdio.h>
+int main ()
+{
+  int arr [5];
+  int p=arr;
+for(int i=0;i<5;i++){
+scanf("%d",p+i);
+}
+for(int i=0;i<5;i++){
+(p+i)=2;
+}
+for(int i=0;i<5;i++)
+{
+printf("%d ",(p+i));
+}
+return 0；
+  }
